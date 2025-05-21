f0876c16ef46825043c442fac8540c2145d20291
leduy343 <duyle7485@gmail.com>
2025-05-22 00:34:36 +0700

    lab 13.2

--- Lab13.1/app/src/main/java/com/example/autocompletetext/MainActivity.java
+++ Lab13.1/app/src/main/java/com/example/autocompletetext/MainActivity.java
@@ -1,11 +1,6 @@
-package com.example.listview;
+package com.example.autocompletetext;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,16 +19,6 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        final String [] arrl = {"iphone","Samsung","XiaoMi","Oppo","Huawei","Sony"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arrl);
-        ListView lsv = findViewById(R.id.lsv);
-        lsv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                TextView txt1 = findViewById(R.id.textView);
-                txt1.setText("Vi tri "+position+" : "+arrl[position]);
-            }
         });
     }
 }
